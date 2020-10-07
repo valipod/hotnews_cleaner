586b67b1a41da4b4da48d9c9fd11407ea9b6f032
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2020-10-07 15:08:23 +0300

    elegantize, so to speak

--- hotnews_cleaner.js
+++ hotnews_cleaner.js
@@ -1,22 +1,23 @@
-var istodor = document.querySelectorAll("a[href='https://www.hotnews.ro/articole_autor/Eugen Istodor']");
-for (var index = 0, length = istodor.length; index < length; index++) {
-  var is = istodor[index];
-  is.parentElement.parentElement.remove();
-}
-var grandparents = ["img[src='https://media.hotnews.ro/media_server1/image-2011-05-13-8621002-0-gsp-logo.jpg']",
-                  "a[href='https://hotnews.maps.arcgis.com/home/"];
-for (var index = 0, length = grandparents.length; index < length; index++) {
-  var gpts = document.querySelectorAll(grandparents[index]);
-  for (var i = 0, l = gpts.length; i < l; i++) {
-    var gpt = gpts[i];
-    gpt.parentElement.remove();
-  };
-}
-var removeClasses = ['forumStudentesc', 'celeMai', 'casutaBloguriDreapta'];
-for (var index = 0, length = removeClasses.length; index < length; index++){
-  var divs = document.getElementsByClassName(removeClasses[index]);
-  for (var i = 0, l = divs.length; i < l; i++) {
-    var div = divs[0];
-    div.remove();
+// to_removes is an array of arrays with selector and number of levels
+// to go up and remove the element
+var to_remove = [
+    ["a[href='https://www.hotnews.ro/articole_autor/Eugen Istodor']", 2],
+    ["img[src='https://media.hotnews.ro/media_server1/image-2011-05-13-8621002-0-gsp-logo.jpg']", 1],
+    ["a[href='https://hotnews.maps.arcgis.com/home/", 1],
+    ['.forumStudentesc', 0],
+    ['.celeMai', 0],
+    ['.casutaBloguriDreapta', 0]
+];
+for (var index = 0, length = to_remove.length; index < length; index++) {
+  var shits = document.querySelectorAll(to_remove[index][0]);
+  for (var i = 0, l = shits.length; i < l; i++) {
+    var shit = shits[i];
+    if (to_remove[index][1] == 0){
+      shit.remove();
+    } else if (to_remove[index][1] == 1){
+      shit.parentElement.remove();
+    } else if (to_remove[index][1] == 2){
+      shit.parentElement.parentElement.remove();
+    };
   };
 }
